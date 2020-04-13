e3e2531149937fc26bd300cac0466b1599267e71
levyxx <levy.b.j.s@gmail.com>
2020-04-12 23:40:19 -0400

    Done EDA of parameter estimates

---
+++ covid_flu/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+
+setup(name="covid_flu",
+      version="1.0",
+      author="Benjamin Levy, Matthieu Meeus, Will Fried, Dimitris Vamvourellis",
+      packages=find_packages())
