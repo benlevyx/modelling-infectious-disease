5e79d96e3b8a28b2f4e20bf3b4255549ebbc44b6
dvamvourellis <dvamvourellis@g.harvard.edu>
2020-05-09 23:45:51 -0700

    added skeleton files for streamlit

---
+++ notebooks/streamlit-app/streamlit_final.py
@@ -0,0 +1,38 @@
+"""Main module for the streamlit app"""
+import streamlit as st
+import flu_eda_streamlit as eda
+import st_data_preprocessing as data_preproc
+import st_homepage as hp
+import st_causal_analysis as ca
+import st_flu_forecasting as flu_forcast
+import covid_forecasting as covid
+import st_conclusion as concl
+
+
+def main():
+    page = st.sidebar.selectbox("Choose a page", ["Homepage",
+                                                  "EDA",
+                                                  "Data Preprocessing",
+                                                  "Causal Analysis",
+                                                  "Flu Forecasting",
+                                                  "COVID-19 Forecasting",
+                                                  "Conclusion"])
+    if page == "Homepage":
+        hp.main()
+    elif page == "EDA":
+        eda.main()
+    elif page == "Data Preprocessing":
+        data_preproc.main()
+    elif page == "Causal Analysis":
+        ca.main()
+    elif page == "Flu Forecasting":
+        flu_forcast.main()
+    if page == "COVID-19 Forecasting":
+        covid.main()
+    elif page == "Conclusion":
+        concl.main()
+
+
+#add about section in the end 
+if __name__ == "__main__":
+    main()
