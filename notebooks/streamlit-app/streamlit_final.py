01496929983d839e9cadf3742d52cb7f56fd806f
Will Fried <wfried@wfried-sf.local>
2020-05-10 17:18:28 -0400

    will

--- notebooks/streamlit-app/streamlit_final.py
+++ notebooks/streamlit-app/streamlit_final.py
@@ -3,7 +3,7 @@ import streamlit as st
 import flu_eda_streamlit as eda
 import st_data_preprocessing as data_preproc
 import st_homepage as hp
-import st_causal_analysis as ca
+import st_flu_inference as inference
 import st_flu_forecasting as flu_forcast
 import covid_forecasting as covid
 import st_conclusion as concl
@@ -13,7 +13,7 @@ def main():
     page = st.sidebar.selectbox("Choose a page", ["Homepage",
                                                   "EDA",
                                                   "Data Preprocessing",
-                                                  "Causal Analysis",
+                                                  "Flu Inference",
                                                   "Flu Forecasting",
                                                   "COVID-19 Forecasting",
                                                   "Conclusion"])
@@ -23,8 +23,8 @@ def main():
         eda.main()
     elif page == "Data Preprocessing":
         data_preproc.main()
-    elif page == "Causal Analysis":
-        ca.main()
+    elif page == "Flu Inference":
+        inference.main()
     elif page == "Flu Forecasting":
         flu_forcast.main()
     if page == "COVID-19 Forecasting":
